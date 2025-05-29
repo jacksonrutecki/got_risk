764e540f20ac43ab2373a5e58bb54e6140b76ba1
Jackson Rutecki <jackson.rutecki@gmail.com>
2025-05-29 18:29:54 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Alert from "./components/Alert";
+
+function App() {
+  return (
+    <div>
+      <Alert>
+        Hello <h1>world</h1>world
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message() {
+    const name = "Jackson";
+    return <h1>Hello {name}!</h1>;
+}
+
+export default Message;
